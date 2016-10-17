a4d36dc09b60b85c5f9abbefcb912a6fc63ef525
rayburgemeestre <ray.burgemeestre@gmail.com>
2016-10-17 18:49:09 +0200

    Freeze the version for V8 for now in Starcry

--- build-v8.sh
+++ build-v8.sh
@@ -6,6 +6,10 @@ fetch --no-history v8
 cd v8
 gclient sync
 
+git checkout 55da769bafac8b531cd8b388df56161b8b7c6416 # something got broken after this commit and 17th of October 2016
+# the libplatform.a was not generated anymore (only the .so), the new build system (gn) does build it, but then other 
+# stuff is missing and linking fails.. so for now I will stick with this hash for my project..
+
 GYP_DIR=build
 if [ ! -f "$GYP_DIR"/gyp_v8 ] ; then
   GYP_DIR=gypfiles
