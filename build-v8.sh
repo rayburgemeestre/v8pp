1fca4a66d9c685c285f2ed6ccadec2b3222011b7
Ray Burgemeestre <ray.burgemeestre@brightcomputing.com>
2018-07-18 09:00:16 +0200

    Change build from gyp -> gn (experimental)

--- build-v8.sh
+++ build-v8.sh
@@ -11,10 +11,17 @@ if [ ! -f "$GYP_DIR"/gyp_v8 ] ; then
   GYP_DIR=gypfiles
 fi
 
-GYP_GENERATORS=make "$GYP_DIR"/gyp_v8 --generator-output=out --depth=. -I"$GYP_DIR"/standalone.gypi -I../v8_options.gypi "$GYP_DIR"/all.gyp
-make v8 v8_libplatform -C out BUILDTYPE=Release -j8 builddir=$(pwd)/out/Release
-mkdir -p lib
-cp out/Release/lib.target/*.so lib
-cp out/Release/lib* lib
-cd ..
+# Google got rid of this gyp stuff: https://chromium-review.googlesource.com/c/v8/v8/+/897566
+# Looks like we have to use GN now: https://github.com/v8/v8/wiki/Building-with-GN
+#GYP_GENERATORS=make "$GYP_DIR"/gyp_v8 --generator-output=out --depth=. -I"$GYP_DIR"/standalone.gypi -I../v8_options.gypi "$GYP_DIR"/all.gyp
+#make v8 v8_libplatform -C out BUILDTYPE=Release -j8 builddir=$(pwd)/out/Release
+./tools/dev/gm.py x64.release
 
+mkdir -p lib
+#cp out/Release/lib.target/*.so lib
+#cp out/Release/lib* lib
+cp out/x64.release/obj/*.a lib
+cp out/x64.release/obj/*.so lib
+cp out/x64.release/obj/libv8* lib
+cd ..
+#./v8/out/x64.release/obj/libv8_libbase.a
