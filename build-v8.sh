50f17a5ed4156bd7c7bc07438e3ad97ab0b1740b
Ray Burgemeestre <ray.burgemeestre@brightcomputing.com>
2018-07-30 10:08:14 +0200

    Freeze v8 to version 6.9.427.6

--- build-v8.sh
+++ build-v8.sh
@@ -4,7 +4,10 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=`pwd`/depot_tools:"$PATH"
 fetch --no-history v8
 cd v8
-gclient sync
+gclient sync -r 6.9.427.6  # 55da769bafac8b531cd8b388df56161b8b7c6416
+
+# old version
+#git checkout 5.5.372.7
 
 GYP_DIR=build
 if [ ! -f "$GYP_DIR"/gyp_v8 ] ; then
@@ -13,15 +16,19 @@ fi
 
 # Google got rid of this gyp stuff: https://chromium-review.googlesource.com/c/v8/v8/+/897566
 # Looks like we have to use GN now: https://github.com/v8/v8/wiki/Building-with-GN
+
+# OLD
 #GYP_GENERATORS=make "$GYP_DIR"/gyp_v8 --generator-output=out --depth=. -I"$GYP_DIR"/standalone.gypi -I../v8_options.gypi "$GYP_DIR"/all.gyp
 #make v8 v8_libplatform -C out BUILDTYPE=Release -j8 builddir=$(pwd)/out/Release
+
+# NEW
 ./tools/dev/gm.py x64.release
 
 mkdir -p lib
 #cp out/Release/lib.target/*.so lib
 #cp out/Release/lib* lib
-cp out/x64.release/obj/*.a lib
-cp out/x64.release/obj/*.so lib
-cp out/x64.release/obj/libv8* lib
+#cp out/x64.release/obj/*.a lib
+#cp out/x64.release/obj/*.so lib
+#cp out/x64.release/obj/libv8* lib
 cd ..
 #./v8/out/x64.release/obj/libv8_libbase.a
