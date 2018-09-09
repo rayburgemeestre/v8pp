7704bf512bb12b6facc3ce811ee59f29fc784cc0
Ray Burgemeestre <ray.burgemeestre@brightcomputing.com>
2018-09-09 19:28:19 +0200

    Fix the build for the Xth time.

--- build-v8.sh
+++ build-v8.sh
@@ -2,19 +2,24 @@
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=`pwd`/depot_tools:"$PATH"
-fetch --no-history v8
+#fetch --no-history v8
+# let's try with history
+fetch v8
 cd v8
 #gclient sync -r 6.9.427.6  # 55da769bafac8b531cd8b388df56161b8b7c6416
-gclient sync -r 7.1.69     # 
+# gclient sync -r 7.1.69     # 
+#gclient sync -r lkgr/6.9
+
+# newest version I found supported via v8pp CI:
+gclient sync -r 6.9.427.13  # https://ci.appveyor.com/project/pmed/v8pp
 
 # old version
 #git checkout 5.5.372.7
 
-GYP_DIR=build
-if [ ! -f "$GYP_DIR"/gyp_v8 ] ; then
-  GYP_DIR=gypfiles
-fi
-
+#GYP_DIR=build
+#if [ ! -f "$GYP_DIR"/gyp_v8 ] ; then
+#  GYP_DIR=gypfiles
+#fi
 # Google got rid of this gyp stuff: https://chromium-review.googlesource.com/c/v8/v8/+/897566
 # Looks like we have to use GN now: https://github.com/v8/v8/wiki/Building-with-GN
 
@@ -24,6 +29,10 @@ fi
 
 # NEW
 # ./build/install-build-deps.sh
+./tools/dev/gm.py x64.release
+
+# workaround, see notes.txt
+echo "v8_enable_embedded_builtins = false" >> cat out/x64.release/args.gn
 ./tools/dev/gm.py x64.release
 
 #mkdir -p lib
